37215212b73daa1eafb4a8418733286332c9740e
ruchipatel <ruchi1992.patel@gmail.com>
2022-05-26 13:48:17 +0530

    Add sticky nav to header

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,10 +5,11 @@ export class NavBar extends Component {
 
 
   render() {
+    
     return (
       <div>
           <div>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-5">
                 <div className="container-fluid">
                 <Link className="navbar-brand" to="/">NewsMonkey</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
